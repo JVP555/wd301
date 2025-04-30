b0a437db8b4adc9ce1e6e32f0e16b9500db7a8de
JVP555 <jilvarghese.2005@gmail.com>
2025-05-01 03:57:55 +0530

    l5 redirect to home after signup

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -1,14 +1,19 @@
-import { useNavigate } from 'react-router-dom';
+
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
   const navigate = useNavigate();
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
       <button
         id="backToHomeButton"
-        onClick={() => navigate('/')}
+        onClick={() => {
+          const isAuthenticated = localStorage.getItem("authenticated");
+          if (isAuthenticated === "true") {
+            navigate("/home");
+          } else navigate("/")}
+        }
         className="px-4 py-2 bg-blue-600 text-white rounded hover:cursor-pointer"
       >
         Back to Home
